95795c45ce5cd045a1a302ca9fc214c21dde96ab
Max Eshleman <meshleman@pivotal.io>
2018-10-03 14:11:19 -0600

    specifically get ginkgo so it installs
    Signed-off-by: Peter Stone <pstone@pivotal.io>

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 export GOPATH="$(cd $(dirname $0)/../../../../..; pwd -P)"
 
 pushd "$(dirname $0)/.."
+  go get github.com/onsi/ginkgo/ginkgo
   go get -t ./...
   $GOPATH/bin/ginkgo -r -p -race -randomizeAllSpecs
 popd
